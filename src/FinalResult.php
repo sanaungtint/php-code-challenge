fad3e609b5b9d6acfb80734dda321d98bd2b9f99
sanaungtint <sanaungtint23@gmail.com>
2023-02-21 01:39:29 +0630

    Refactor code

--- src/FinalResult.php
+++ src/FinalResult.php
@@ -1,40 +1,80 @@
 <?php
 
+include 'constants/CsvConstant.php';
+
+/**
+ * Class FinalResult
+ */
 class FinalResult {
-    function results($f) {
-        $d = fopen($f, "r");
-        $h = fgetcsv($d);
-        $rcs = [];
-        while(!feof($d)) {
-            $r = fgetcsv($d);
-            if(count($r) == 16) {
-                $amt = !$r[8] || $r[8] == "0" ? 0 : (float) $r[8];
-                $ban = !$r[6] ? "Bank account number missing" : (int) $r[6];
-                $bac = !$r[2] ? "Bank branch code missing" : $r[2];
-                $e2e = !$r[10] && !$r[11] ? "End to end id missing" : $r[10] . $r[11];
-                $rcd = [
-                    "amount" => [
-                        "currency" => $h[0],
-                        "subunits" => (int) ($amt * 100)
-                    ],
-                    "bank_account_name" => str_replace(" ", "_", strtolower($r[7])),
-                    "bank_account_number" => $ban,
-                    "bank_branch_code" => $bac,
-                    "bank_code" => $r[0],
-                    "end_to_end_id" => $e2e,
-                ];
-                $rcs[] = $rcd;
+
+    /**
+     * @param $file
+     * @return array
+     * @throws Exception
+     */
+    public function results($file): array
+    {
+        $handle = fopen($file, "r");
+        if ($handle === false) {
+            throw new Exception("Unable to open file");
+        }
+
+        $headers = fgetcsv($handle);
+        if ($headers === false) {
+            throw new Exception("Unable to read header row");
+        }
+
+        $records = $this->processRows($handle, $headers);
+
+        fclose($handle);
+        return [
+            "filename" => basename($file),
+            "document" => $handle,
+            "failure_code" => $headers[CsvConstant::FAILURE_CODE],
+            "failure_message" => $headers[CsvConstant::FAILURE_MESSAGE],
+            "records" => $records
+        ];
+    }
+
+    /**
+     * @param $handle
+     * @param $headers
+     * @return array
+     */
+    private function processRows($handle, $headers): array
+    {
+        $records = [];
+        while (!feof($handle)) {
+            $row = fgetcsv($handle);
+            if(count($row) == 16) {
+                $records[] = $this->processRow($row, $headers);
             }
         }
-        $rcs = array_filter($rcs);
+        return $records;
+    }
+
+    /**
+     * @param $row
+     * @param $headers
+     * @return array
+     */
+    private function processRow($row, $headers): array
+    {
+        $amount = !$row[CsvConstant::AMOUNT] || $row[CsvConstant::AMOUNT] == "0" ? 0 : (float) $row[CsvConstant::AMOUNT];
+        $bankAccountNumber = !$row[CsvConstant::BANK_ACCOUNT_NUMBER] ? "Bank account number missing" : (int) $row[CsvConstant::BANK_ACCOUNT_NUMBER];
+        $bankBranchCode = !$row[CsvConstant::BANK_BRANCH_CODE] ? "Bank branch code missing" : $row[CsvConstant::BANK_BRANCH_CODE];
+        $endToEndId = !$row[CsvConstant::END_TO_END_ID_FIRST] && !$row[CsvConstant::END_TO_END_ID_LAST] ? "End to end id missing" : $row[CsvConstant::END_TO_END_ID_FIRST] . $row[CsvConstant::END_TO_END_ID_LAST];
+
         return [
-            "filename" => basename($f),
-            "document" => $d,
-            "failure_code" => $h[1],
-            "failure_message" => $h[2],
-            "records" => $rcs
+            "amount" => [
+                "currency" => $headers[CsvConstant::CURRENCY],
+                "subunits" => (int) ($amount * 100)
+            ],
+            "bank_account_name" => str_replace(" ", "_", strtolower($row[CsvConstant::BANK_ACCOUNT_NAME])),
+            "bank_account_number" => $bankAccountNumber,
+            "bank_branch_code" => $bankBranchCode,
+            "bank_code" => $row[CsvConstant::BANK_CODE],
+            "end_to_end_id" => $endToEndId,
         ];
     }
 }
-
-?>
---
+++ src/constants/CsvConstant.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Class CsvConstant
+ */
+class CsvConstant {
+
+    // header row
+    const CURRENCY = 0;
+    const FAILURE_CODE = 1;
+    const FAILURE_MESSAGE = 2;
+
+    // content row
+    const BANK_CODE = 0;
+    const BANK_BRANCH_CODE = 2;
+    const BANK_ACCOUNT_NUMBER = 6;
+    const BANK_ACCOUNT_NAME = 7;
+    const AMOUNT = 8;
+    const END_TO_END_ID_FIRST = 10;
+    const END_TO_END_ID_LAST = 11;
+}
